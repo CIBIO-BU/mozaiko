dcc295cbc79aa21c116955cf35ec8f29274f2d02
Camila Babo <anacamilarb@gmail.com>
2024-07-24 16:10:52 +0100

    ADD: .gitleaks.toml to whitelist genetic sequences + scripts to pre-process fasta file + processed fasta files Refactor: utility_scripts

---
+++ external-scripts/diat-barcode_pre-process/excel2tsv.py
@@ -0,0 +1,27 @@
+"""
+This is a stand-alone script to convert an excel file to a tsv file.
+"""
+import pandas as pd
+
+def excel2tsv(excel_file, sheet_name, tsv_file):
+    """
+    This function converts an excel file to a tsv file.
+
+    Parameters
+    excel_file (str): The input excel file.
+    sheet_name (str): The sheet name in the excel file to be converted.
+    tsv_file (str): The output tsv file.
+    """
+    df = pd.read_excel(excel_file, sheet_name=sheet_name)
+    df.to_csv(tsv_file, sep='\t', index=False)
+
+    print("Excel file converted to TSV file successfully.")
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Convert an excel file to a tsv file.')
+    parser.add_argument('--excel_file', '-e', help='The input excel file.')
+    parser.add_argument('--sheet_name', '-s', help='The sheet name in the excel file.')
+    parser.add_argument('--tsv_file', '-t', help='The output tsv file.')
+    args = parser.parse_args()
+    excel2tsv(args.excel_file, args.sheet_name, args.tsv_file)
---
+++ external-scripts/diat-barcode_pre-process/tsv2fasta.py
@@ -0,0 +1,67 @@
+"""
+This is a stand-alone script to convert a TSV file to a FASTA file; and remove alignment dashes from FASTA files.
+"""
+import pandas as pd
+import subprocess
+
+def tsv2fasta(tsv_file, fasta_file):
+    """
+    This function converts a TSV file to a FASTA file.
+
+    Parameters
+    tsv_file (str): The input TSV file.
+    fasta_file (str): The output FASTA file.
+    """
+    df = pd.read_csv(tsv_file, sep='\t')
+    sequences = df['sequence']
+    seq_ids = df['seq_ID']
+    species = df['species']
+    with open(fasta_file, 'w', encoding='UTF-8') as f:
+        for i, sequence in enumerate(sequences):
+            f.write(f'>{seq_ids[i]}|{species[i]}\n')
+            f.write(f'{sequence}\n')
+
+    print("TSV file converted to FASTA file successfully.")
+
+def remove_alignment_dashes(fasta_file, output_fasta_file):
+    """
+    This function removes alignment dahses from FASTA files.
+
+    These alignment dashes '-' are added to the sequences to align their lengths.
+
+    Parameters
+    fasta_file (str): The input FASTA file.
+    output_fasta_file (str): The output FASTA file.
+    """
+    with open(fasta_file, 'r', encoding='UTF-8') as f:
+        lines = f.readlines()
+    with open(output_fasta_file, 'w', encoding='UTF-8') as f:
+        for i, line in enumerate(lines):
+            if i % 2 == 0:
+                f.write(line)
+            else:
+                f.write(line.replace('-', ''))
+    
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Convert a TSV file into a FASTA file')
+    parser.add_argument('--tsv_file', '-t',
+                        help='The input TSV file')
+    parser.add_argument('--fasta_file', '-f',
+                        help='The ouput FASTA file.')
+    parser.add_argument('--output_fasta_file', '-o',
+                       help='The output FASTA file without alignment dashes.')
+    parser.add_argument('--task', '-a', choices=['tsv2fasta', 'remove_alignment_dashes'], 
+                        help='The task to perform: tsv2fasta or remove_alignment_dashes')
+    args = parser.parse_args()
+
+    if args.task == 'tsv2fasta':
+        tsv2fasta(args.tsv_file, args.fasta_file)
+    elif args.task == 'remove_alignment_dashes':
+        remove_alignment_dashes(args.fasta_file, args.output_fasta_file)
+    else:
+        print("Invalid task." +
+              "Please provide a valid task ('tsv2fasta', 'remove_alignment_dashes') " +
+              "with the use of -a or --task argument.")
+
+    
