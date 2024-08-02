6ea9c8af07f04e3e7c7635f1f77a8ae5e8af64e0
Camila Babo <anacamilarb@gmail.com>
2024-08-02 16:03:43 +0100

    fix lint conflicts

--- tests/test_custom_fasta_import.py
+++ tests/test_custom_fasta_import.py
@@ -11,7 +11,6 @@ import pandas as pd
 
 from src.reference_database.sequence_import import CustomFastaImport, LineageFileLoader
 
-
 class TestCustomFastaImport(unittest.TestCase):
     """
     Class to test the CustomFastaImport class.
