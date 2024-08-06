1c8014bb5f9ddf52c9df39657ca14232efaae780
Camila Babo <anacamilarb@gmail.com>
2024-08-06 13:14:37 +0100

    conda build not working rm associated files

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='dnaquaimg',
-    version='0.1',
-    packages=find_packages(include=['src', 'src.*']),
-    url='https://github.com/CIBIO-BU/DNAquaIMG'
-)
