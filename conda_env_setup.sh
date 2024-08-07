85320203159f22a5d044251f3c81322076232e32
Camila Babo <anacamilarb@gmail.com>
2024-08-07 16:42:07 +0100

    fix cionda_env_setup.py according to bu_python

--- conda_env_setup.sh
+++ conda_env_setup.sh
@@ -14,19 +14,26 @@ check_conda() {
 
 # Check if env already exists
 check_env() {
-    if conda env list | grep ".*${ENV_NAME}.*" >/dev/null 2>&1; then
+    if conda env list | grep -q "${ENV_NAME}"; then
         echo "Conda environment '$ENV_NAME' already exists. Skipping environment creation."
     else
         echo "Creating Conda environment: $ENV_NAME"
-        conda env create -f environment.yml
+        conda env create -f environment.yml || { echo "Failed to create Conda environment"; exit 1; }
     fi
 }
 
 # Activate Environment
 activate_env() {
     echo "Activating Conda environment: $ENV_NAME"
-    source $HOME/miniconda/etc/profile.d/conda.sh || { echo "Failed to source Conda script"; exit 1; }
-    conda activate $ENV_NAME
+
+    # Ensure Conda is initialized
+    # This step might be needed to properly configure the shell for Conda
+    CUR_SHELL=shell.$(basename -- "${SHELL}")
+    eval "$(conda "$CUR_SHELL" hook)"
+
+    set -e
+    conda activate "$ENV"
+    echo "INFO: conda environment $ENV created and activated"
 }
 
 # Clone repository
@@ -35,17 +42,17 @@ clone_repo() {
         echo "Directory $PACKAGE_DIR already exists. Skipping repository cloning."
     else
         echo "Cloning repository from $REPO_URL."
-        git clone $REPO_URL
+        git clone "$REPO_URL" || { echo "Failed to clone repository"; exit 1; }
     fi
 }
 
 # Install Package
 install_package() {
     echo "Navigating to package directory: $PACKAGE_DIR"
-    cd $PACKAGE_DIR || exit
+    cd "$PACKAGE_DIR" || { echo "Directory $PACKAGE_DIR does not exist"; exit 1; }
 
     echo "Installing package"
-    pip install .
+    pip install . || { echo "Failed to install package"; exit 1; }
 
     echo "Installation complete."
 }
