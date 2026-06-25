2b7c0667a45d41ba9af50845828d0e745379a919
Camila Babo <anacamilarb@gmail.com>
2026-06-25 12:18:28 +0100

    New version 0.1.9

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 ## mozaiko
-VERSION=0.1.8
+VERSION=0.1.9
 
 release:
 	# Update version in pyproject.toml
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="mozaiko",
-    version="0.1.8",
+    version="0.1.9",
     author="Camila Babo",
     author_email="camila.babo@cibio.up.pt",
     url="https://github.com/CIBIO-BU/mozaiko",
